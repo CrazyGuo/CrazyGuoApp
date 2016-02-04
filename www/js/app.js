c3cfc3064d2134b3e3e5ebc140c110178c7108c0
CrazyGuo <guohuanhuan_cool@126.com>
2016-02-04 14:05:56 +0800

    show tabs in bottom

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('ionicApp', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,34 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+.config(["$stateProvider",  "$urlRouterProvider", "$ionicConfigProvider", "$sceDelegateProvider", "$compileProvider",function (e, t, n, i, r) {
+        n.platform.ios.tabs.style("standard"),
+        n.platform.ios.tabs.position("bottom"),
+        n.platform.android.tabs.style("standard"),
+        n.platform.android.tabs.position("bottom"),
+        n.platform.ios.navBar.alignTitle("center"),
+        n.platform.android.navBar.alignTitle("center"),
+        n.platform.ios.views.transition("ios"),
+        n.platform.android.views.transition("android"),
+        n.backButton.text(""),
+        n.backButton.previousTitleText(!1),
+        n.views.maxCache(5),
+
+        r.aHrefSanitizationWhitelist(/^\s*(https?|http|ftp|mailto|chrome-extension|tel|file):/),
+        i.resourceUrlWhitelist(["self", "http://*.youku.**", "http://*.tudou.**", "http://*.ddc.**", "http://*.cnev.**"]),
+
+        e.state("tabs", {
+                url: "/tab",
+                "abstract": !0,
+                templateUrl: "templates/tabs.html"
+            }).state("tabs.home", {
+                url: "/home",
+                views: {
+                    "home-tab": {
+                        templateUrl: "templates/home.html"
+                    }
+                }
+            });
+
+        t.otherwise("tab/home");
+}]);
